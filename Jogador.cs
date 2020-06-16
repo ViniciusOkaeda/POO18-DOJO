7e3d4d948994de1805cd9c0a461394be94695b19
Vinicius <viniciusokaeda@gmail.com>
2020-06-16 15:33:01 -0300

    estrutura inicial feita

---
+++ Jogador.cs
@@ -0,0 +1,13 @@
+namespace AULA18DOJOFIX
+{
+    public class Jogador
+    {
+        public string nome { get ; set ;}
+
+        public string posicao { get ; set;}
+        
+        public float idade { get ; set ;}
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AULA18DOJOFIX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Jogador vinicius = new Jogador();
+
+            System.Console.WriteLine("Digite seu nome");
+            string nome = Console.ReadLine();
+
+
+        }
+    }
+}
